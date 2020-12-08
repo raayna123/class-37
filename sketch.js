85203c11744b2f2476e48dd8d515aeb680dc19fd
raayna123 <72254862+raayna123@users.noreply.github.com>
2020-12-08 15:32:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var database;
+var gameState = 0;
+var playerCount, form, player, game;
+var allPlayers;
+var car1, car2, car3, car4;
+var cars;
+
+function setup(){
+    database = firebase.database();
+
+    createCanvas(displayWidth - 20,displayHeight - 30);
+
+    game = new Game();
+    game.getState();
+    game.start();
+
+}  
+
+function draw(){
+    background("white");
+    if(playerCount == 4) {
+        game.update(1);
+    }
+    if(gameState == 1) {
+        game.play();
+    }
+}
+
+
+
+
